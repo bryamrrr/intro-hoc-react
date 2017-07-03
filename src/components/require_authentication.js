24e20f49d4e1c7ec0277afa1d6db343b14f289ad
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-07-02 20:22:02 -0500

    Creating a higher order component

---
+++ src/components/require_authentication.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default (ComposedComponent) => {
+  class Authentication extends Component {
+    render() {
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}
---
+++ src/components/resources.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Hola mundo, esta página es privada!
+      <ul>
+        <li>1 litro de leche</li>
+        <li>1 litro de yogurt</li>
+        <li>1 kilo de manzanas</li>
+      </ul>
+    </div>
+  );
+}
